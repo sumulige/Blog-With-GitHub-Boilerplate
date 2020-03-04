cc774d57a6f6a9be1a4f412cae9148f31b25a84a
苏木力格 <30450613+sumulige@users.noreply.github.com>
2020-03-04 14:47:07 +0800

    Update conf.py
    修改为自己的一些信息

--- conf.py
+++ conf.py
@@ -21,12 +21,12 @@ enable_jsdelivr = {
 # 站点设置
 site_name = "我的个人博客"
 site_logo = "${static_prefix}logo.png"
-site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
+site_build_date = "2020-03-04T16:51+08:00"
+author = "sumulige"
+email = "sumulige@163.com"
 author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+description = "道，境界也。"
+key_words = ['Maverick', 'sumulige', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
     {
